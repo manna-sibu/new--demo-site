e56e57c172dfab1d005524557d3b9cc82e08bb01
sibu Manna <sibubluehorse@gmail.com>
2025-04-12 11:05:21 +0530

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   let marquee = document.getElementById("marquee");
+
+//   marquee.addEventListener("mouseover", function () {
+//     marquee.stop();
+//   });
+
+//   marquee.addEventListener("mouseout", function () {
+//     marquee.start();
+//   });
+// });
+
+
